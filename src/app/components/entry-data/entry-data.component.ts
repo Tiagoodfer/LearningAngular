edb6a67e0c808c7e28a89c0e4ffea16d66cfa987
tiago <tiagodfer@icloud.com>
2025-11-14 23:56:01 -0300

    first commit

---
+++ src/app/components/entry-data/entry-data.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-entry-data',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './entry-data.component.html',
+  styleUrls: ['./entry-data.component.css']
+})
+export class EntryDataComponent {
+
+}
